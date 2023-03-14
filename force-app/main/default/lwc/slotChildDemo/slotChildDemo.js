e3cae13484cdb1e66be563bf534919af05975429
Lalith Aditya <lalithkumaraditya@gmail.com>
2023-03-14 14:40:03 +0530

    added resuable component

--- force-app/main/default/lwc/slotChildDemo/slotChildDemo.js
+++ force-app/main/default/lwc/slotChildDemo/slotChildDemo.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class SlotChildDemo extends LightningElement {}
+export default class SlotChildDemo extends LightningElement {
+    handleFooterChange() {
+        const footerelem = this.template.querySelector('.slds-card__footer')
+        if (footerelem) {
+            footerelem.classList.remove('slds-hide')
+        }
+    }
+}
